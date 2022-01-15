2432dbe40ededf4be804dfb1e4136f856774c1bb
Kern Jackson <kernjackson@gmail.com>
2022-01-15 11:01:04 -0600

    Initial Commit

---
+++ ToggleSideBar/ToggleSideBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleSideBarApp.swift
+//  ToggleSideBar
+//
+//  Created by Kern Jackson on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ToggleSideBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
